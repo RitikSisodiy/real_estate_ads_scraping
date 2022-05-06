2e1c7438926724de41252bc69daf52796188734d
ritik <ritik@kifwat.com>
2022-05-06 17:32:17 +0530

    fix pap

--- real_estate_advert/pap/scraper.py
+++ real_estate_advert/pap/scraper.py
@@ -228,6 +228,8 @@ def pap_scraper():
     producer = KafkaTopicProducer()
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
+
     # initialize chrome and search the URL
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
     driver.get(URL)
