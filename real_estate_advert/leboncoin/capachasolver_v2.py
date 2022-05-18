f1b2c5960f5c57fce660488dfc537299ba2b0cf3
Tasaqi <38526341+Tasaqi@users.noreply.github.com>
2022-05-18 19:51:05 +0500

    The leboncoin and selgoer scraper are developed and added to API

---
+++ real_estate_advert/leboncoin/capachasolver_v2.py
@@ -0,0 +1,265 @@
+from ast import While
+from xml.dom import ValidationErr
+from selenium import webdriver
+import time
+import sys
+import undetected_chromedriver as uc
+import traceback
+from selenium.webdriver import DesiredCapabilities
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from AudioToText import GetAudioToNumber
+from urllib.parse import unquote
+import json
+import os
+headless=True
+currentpath = os.getcwd()
+chromedirver = ChromeDriverManager().install()
+try:
+    vpn = sys.argv[1]
+    ip = sys.argv[2]
+except:
+    vpn = False 
+    ip=None
+def saveCookies(cookies):
+    cookie = ""
+    for data in cookies:
+        if data['name'] == "atuserid":
+            pass
+        else:
+            cookie += data['name']+"="+unquote(data['value'])+"; "
+    final  = ""
+    with open("cookies.json",'r') as file:
+        data = file.readlines()
+        if len(data)>=8:
+            for i in range(3,7):
+                final += data[i]
+    if final:
+        with open("cookies.json",'w') as file:
+            file.write(final)
+    with open("cookies.json",'a') as file:
+        file.write(cookie+"\n")
+# def getdriver(proxy=None):
+#     options = Options()
+#     options.add_argument('--no-sandbox')
+#     options.add_argument('--start-maximized')
+#     options.add_argument('--single-process')
+#     if headless:
+#         options.add_argument('--headless')
+#     options.add_argument('--disable-dev-shm-usage')
+#     options.add_argument('window-size=1920x1080')
+#     options.add_argument("--incognito")
+#     options.add_argument('--disable-blink-features=AutomationControlled')
+#     # options.add_argument('--disable-blink-features=AutomationControlled')
+#     options.add_argument("--mute-audio")
+#     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
+#     options.add_experimental_option('useAutomationExtension', False)
+
+#     options.add_experimental_option("excludeSwitches", ["enable-automation"])
+#     if proxy:
+#         options.add_argument('--proxy-server=%s' % proxy)
+#     capabilities = DesiredCapabilities.CHROME.copy()
+#     capabilities['acceptSslCerts'] = True 
+#     capabilities['acceptInsecureCerts'] = True
+#     driver = webdriver.Chrome(f"{currentpath}/chromedriver",options=options,desired_capabilities=capabilities)
+#     # driver = webdriver.Chrome('./chromedriver',options=options)
+#     return driver
+
+
+# def getdriver(proxy=None):
+#     #! Dont change anything
+#     chromeOptions = uc.ChromeOptions()
+#     # chromeOptions.add_argument("--no-sandbox");
+#     # chromeOptions.add_argument("--disable-dev-shm-usage");
+#     # chromeOptions.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36")
+
+#     # # chromeOptions.add_argument("--no-sandbox")
+#     # # chromeOptions.add_argument("--headless")
+#     # chromeOptions.add_argument("--disable-infobars")
+#     # chromeOptions.add_argument("enable-automation");
+#     # chromeOptions.headless=True
+#     if headless:
+#         chromeOptions.add_argument("--headless");
+#     # # chromeOptions.add_argument("--window-size=1920,1080");
+#     # chromeOptions.add_argument("--no-sandbox");
+#     # chromeOptions.add_argument("--disable-extensions");
+#     # chromeOptions.add_argument("--dns-prefetch-disable");
+#     # chromeOptions.add_argument("--disable-gpu");
+#     # # chromeOptions.add_argument("--disable-gpu")
+#     # chromeOptions.add_argument("--disable-extensions")
+#     # chromeOptions.add_argument("--mute-audio")
+#     # chromeOptions.add_experimental_option(
+#     #     'excludeSwitches', ['enable-logging'])
+#     # chromeOptions.add_experimental_option("prefs", {"profile.default_content_setting_values.media_stream_mic": 2,
+#     #                                                 "profile.default_content_setting_values.media_stream_camera": 2,
+#     #                                                 "profile.default_content_setting_values.geolocation": 2,
+#     #                                                 "profile.default_content_setting_values.notifications": 2
+#     #                                                 })
+#     if proxy:
+#         chromeOptions.add_argument('--proxy-server=%s' % proxy)
+
+#     driver = uc.Chrome(options=chromeOptions)
+#     return driver
+
+def getdriver(proxy=None):
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--start-maximized')
+    options.add_argument('--single-process')
+    if headless:
+        options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--incognito")
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument("--mute-audio")
+    options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36")
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    if proxy:
+        options.add_argument('--proxy-server=%s' % proxy)
+    driver = webdriver.Chrome(chromedirver,options=options)
+    return driver
+
+def solveCapcha(driver):
+    a = 6
+    count = 0
+    while True:
+        try:
+            # count +=1
+            # if vpn=="tor":
+            #     print("working with tor")
+            #     driver = getdriver(proxy="socks4://127.0.0.1:9050")
+            # # driver = getdriver(proxy="http://41.65.236.44:1981")
+            # # driver = getdriver(proxy="socks4://161.82.252.36:4153")
+            # elif vpn=='notor':
+            #     print("working without tor")
+            #     driver = getdriver()
+            # else:
+            #     pr = ip
+            #     print(pr)
+            #     driver = getdriver(proxy = pr)
+            # url ='''https://www.leboncoin.fr/'''
+            # driver.get(url) 
+            pagetitle = driver.title
+            print("this is the page tile ", pagetitle)
+            if pagetitle == 'leboncoin, site de petites annonces gratuites':
+                cookies = driver.get_cookies()
+                saveCookies(cookies)
+                print(f"{count} cookies saved")
+                # driver.close()
+                return "solved"
+            driver.switch_to.frame(0)
+            wait = WebDriverWait(driver, 25)
+            statustitle = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="captcha-container"]/div[3]/div/div[1]')))
+            print(statustitle.text)
+            wait = WebDriverWait(driver, 25)
+            verify = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="1e505deed3832c02c96ca5abe70df9ab"]/div/div[2]/div[1]')))
+            time.sleep(2)
+            verify.click()
+            audiobutton = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div[2]/div/a[4]')))
+            time.sleep(2)
+            audiobutton.click()
+            time.sleep(1)
+            # audiosrc = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div/div/div[2]/div[2]/audio')))
+            solved= False
+            lastsrc = '1'
+            while not solved:
+                src = ""
+                while not src or src==lastsrc:
+                    if src == lastsrc:
+                        wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]'))).click()
+                        time.sleep(1)
+                    audiosrc = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div/div/div[2]/div[2]/audio')))
+                    time.sleep(1)
+                    src = audiosrc.get_attribute("src")
+                lastsrc = src
+                num = GetAudioToNumber(src)
+                inputbutton = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div/div/div[2]/div[3]/input')))
+                inputbutton.send_keys(num)
+                submit = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div/div/div[2]/div[4]')))
+                time.sleep(1)
+                submit.click()
+                try:
+                    if len(num)==6:
+                        print("in try success chekc")
+                        return True
+                        succes = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="1e505deed3832c02c96ca5abe70df9ab"]/div/div[2]/div[2]/div/div[2]/span[1]')))
+                        solved = True
+                    else:
+                        print("in try success else")
+                        return False
+                        raise ValidationErr
+                except:
+                    return False
+                    try:
+                        print("in try except")
+                        wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div[2]'))).click()
+                        time.sleep(1)
+                    except:solved =True
+                if solved == True:
+                    title = "leboncoin, site de petites annonces gratuites"
+                    print("cheking title :",title)
+                    try:
+                        wait.until(EC.title_is(title))
+                    except:
+                        driver.get(url)
+                        wait.until(EC.title_is(title))
+                    cookies = driver.get_cookies()
+                    saveCookies(cookies)
+                    print(f"{count} cookies saved")
+                    driver.close()
+                    return "solved"
+                    break
+                    time.sleep(100)
+        except Exception as e:
+            print(traceback.format_exc())
+            print(e,"++++++++++++++++++++++++++++==")
+            driver.close()
+            time.sleep(5)
+            pass
+        
+
+
+def isCaptcha(driver):
+    solved = False
+    count =0 
+    isCaptchaPresent =False
+    while True:
+        count += 1
+        if count == 5 : break
+        try: 
+            frames = driver.find_elements(By.TAG_NAME, 'iframe')
+            for counter, i in enumerate(frames):
+                if 'geo.captcha-delivery.com' in i.get_attribute('src'):
+                    isCaptchaPresent = True
+                    solved = solveCapcha(driver)
+                    if solved == "solved":
+                        print('Captcha must have been solved')
+                        return True
+                        
+                    else: 
+                        print(f'for loop break {solved}')
+                        break
+        except:
+            print('Captcha Error')
+            continue
+    return isCaptchaPresent
+
+if __name__ == "__main__":
+    def getStatus():
+        with open("status.txt",'r') as file:
+            return file.read()
+    while True:
+        while True:
+            time.sleep(5)
+            status = getStatus()
+            print(status,"this is status")
+            if status == "403":
+                solveCapcha()
+                time.sleep(20)
+                
+                
---
+++ real_estate_advert/leboncoin/scraper.py
@@ -0,0 +1,253 @@
+try:
+    import sys
+    import pickle
+    import os
+    # from webdriver_manager.chrome import ChromeDriverManager
+    # from fp.fp import FreeProxy
+    from fake_useragent import UserAgent
+    from bs4 import BeautifulSoup
+    from selenium import webdriver
+    import random
+    from selenium.webdriver import Chrome
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.common.action_chains import ActionChains
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.common.exceptions import TimeoutException, NoSuchElementException
+    import undetected_chromedriver as uc
+    import time
+    from capachasolver_v2 import isCaptcha
+    from kafka_publisher import KafkaTopicProducer
+    print('all module are loaded ', os.getcwd())
+except Exception as e:
+    print("Error ->>>: {} ".format(e))
+
+global driverinstance
+driverinstance = None
+producer = KafkaTopicProducer()
+
+
+class Spoofer(object):
+
+    def __init__(self, country_id=['FR'], rand=True, anonym=True):
+        self.country_id = country_id
+        self.rand = rand
+        self.anonym = anonym
+        self.userAgent, self.ip = self.get()
+
+    def get(self):
+        ua = UserAgent()
+        ip = f"169.57.1.85:8123"
+        return ua.random, ip
+
+
+class DriverOptions(object):
+
+    def __init__(self):
+        # self.options = Options()
+        self.options = uc.ChromeOptions()
+        self.options.add_argument('--disable-blink-features=AutomationControlled')
+        self.options.add_argument('--disable-popup-blocking')
+        # self.options.add_argument('--no-sandbox')
+        # self.options.add_argument('--start-maximized')
+        # # self.options.add_argument('--start-fullscreen')
+        # self.options.add_argument('--single-process')
+        # self.options.add_argument('--disable-dev-shm-usage')
+        self.options.add_argument("--incognito")
+        self.options.add_argument("--headless")
+        # self.options.add_argument('--disable-blink-features=AutomationControlled')
+        # self.options.add_argument('--disable-blink-features=AutomationControlled')
+        # self.options.add_experimental_option('useAutomationExtension', False)
+        # self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # self.options.add_argument("disable-infobars")
+
+        self.helperSpoofer = Spoofer()
+
+        self.options.add_argument('user-agent={}'.format(self.helperSpoofer.userAgent))
+        # self.options.add_argument('--proxy-server=%s' % self.helperSpoofer.ip)
+
+
+class WebDriver(DriverOptions):
+
+    def __init__(self, path=''):
+        DriverOptions.__init__(self)
+        self.driver_instance = self.get_driver()
+
+    def get_driver(self):
+        print("""
+        IP:{}
+        UserAgent: {}
+        """.format(self.helperSpoofer.ip, self.helperSpoofer.userAgent))
+
+        # PROXY = self.helperSpoofer.ip
+        # webdriver.DesiredCapabilities.CHROME['proxy'] = {
+        #     "httpProxy": PROXY,
+        #     "ftpProxy": PROXY,
+        #     "sslProxy": PROXY,
+        #     "noProxy": None,
+        #     "proxyType": "MANUAL",
+        #     "autodetect": False
+        # }
+        # webdriver.DesiredCapabilities.CHROME['acceptSslCerts'] = True
+
+        path = os.path.join(os.getcwd(), 'chromedriver')
+
+        # driver = webdriver.Chrom(ChromeDriverManager().install(), options=self.options)
+        # driver = webdriver.Chrome(options=self.options)
+        driver = uc.Chrome(options=self.options)
+
+        return driver
+
+def getStatus():
+    with open("status.txt",'r') as file:
+        return file.read()
+
+    
+def accept_cookies(driver, url):
+    try:
+        # click on accept cookies popup
+        driver.get(url)
+        time.sleep(4)
+        while True:
+            print('solving ...')
+            wait = WebDriverWait(driver, 5)
+            isCaptcha(driver)
+            break
+
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.element_to_be_clickable((By.ID, 'didomi-notice-agree-button')))
+        button =driver.find_element(By.ID, 'didomi-notice-agree-button') 
+        if button:
+            button.click()
+        driver.get_cookies()
+        return True
+    except Exception as e:
+        print(e)
+    return False
+
+def scrape_ad_url(url, driver):
+    ad_data = { "image_urls":[], "3d_view": "", "location": "", "title": "", "price": "",
+            "rooms" : "", "area" : "", "date" : "",
+               "description": "", 
+               "vendor_information": {"vendor_type": "", "vendor_url": "", "vendor_name": "",
+                                      "advertisement": "", "logo_url": "", "fb_url": "",
+                                       "vendor_location" :"", "opening_time": ""}}
+               
+    ## open new page
+    try:
+        driver.execute_script("window.open('');")
+        WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
+        driver.switch_to.window(driver.window_handles[1])
+        driver.get(url)
+        solved = False
+        while not solved:
+            solved = isCaptcha(driver)
+            if solved:
+                driver.get(url)
+            else: 
+                break
+        
+        try:
+            driver.find_element(By.XPATH,'//button[@class="styles_ImageFit__eCNtA styles_cover__Zzp_L styles_fullWidth__r3KVX styles_clickable__YVsVw _3s2p-"]').click()
+            divs = driver.find_elements(By.CLASS_NAME, "slick-slide")
+            for i in divs:
+                img= i.find_element(By.TAG_NAME, 'img').get_attribute('src')
+                if img not in ad_data['image_urls']:
+                    ad_data['image_urls'].append(img)
+            driver.find_element(By.XPATH, '//button[@title="Fermer"]').click()
+        except:
+            print("ERR ----- image")
+            
+        try:
+            ad_data['title'] = driver.find_element(By.XPATH, '//h1[@data-qa-id="adview_title"]').text
+        except:
+            print("ERR ------ title")
+        try:
+            ad_data['rooms'] = driver.find_element(By.XPATH, '//div[@data-qa-id="adview_spotlight_description_container"]/div[2]/div/span').text.strip()
+        except:
+            print("ERR ------ rooms")
+        try:
+            ad_data['area'] = driver.find_element(By.XPATH, '//div[@data-qa-id="adview_spotlight_description_container"]/div[2]/div/span[2]').text.strip()
+        except:
+            print("ERR ------ area")
+        try:
+            ad_data['date'] = driver.find_element(By.XPATH, '//div[@data-qa-id="adview_spotlight_description_container"]/div[3]').text.strip()
+        except:
+            print("ERR ------ date")
+        try:
+            ad_data['price'] = driver.find_element(By.XPATH, '//div[@data-qa-id="adview_spotlight_description_container"]/div[2]/div[3]').text.strip()
+        except:
+            print("ERR ------ price")
+        try:
+            try:
+                driver.find_element(By.XPATH, '//button[@class="_27ngl Roh2X _2NG-q _29R_v HGqCc _3Q3XS Mb3fh _137P- _35DXM P4PEa wEezs"]').click()
+            except:
+                print('')
+            ad_data['description'] = driver.find_element(By.XPATH, '//div[@data-qa-id="adview_description_container"]').text.strip()
+        except:
+            print("ERR ------ description")
+        try:
+            ad_data['vendor_information']['vendor_name'] = driver.find_elements(By.XPATH, '//div[@data-qa-id="adview_contact_container"]//a')[1].text.strip()
+        except:
+            print('vendor name')
+        print(ad_data)
+        producer.kafka_producer_sync(topic="leboncoin-data", data=ad_data)
+        
+        driver.execute_script("window.close();")
+        driver.switch_to.window(driver.window_handles[0])
+    except:
+        print('asdfalksdfj;lasjdkf;lkjasdl;fkj')
+
+        
+
+
+
+def scrape_ads( driver, url):
+    # driver.get(url)
+    while True:
+        a = driver.find_elements(By.CLASS_NAME,'styles_adCard__HQRFN')
+        print(f'{len(a)} urls found')
+        for i in a:
+            scrape_ad_url(i.find_element(By.TAG_NAME, 'a').get_attribute('href'), driver)
+        try:
+            next_page = driver.find_element(By.XPATH, '//a[@title="Page suivante"]').get_attribute('href')
+            driver.get(next_page)
+            driver.implicitly_wait(5)
+            solved = False
+            while not solved:
+                solved = isCaptcha(driver)
+                if solved:
+                    driver.get(url)
+                else: 
+                    break
+
+        except Exception as e:
+            print(e)
+            break
+
+
+        
+def main_scraper(paylaod):
+    paylaod = {'text': 'house', 'min_price': 0.0, 'max_price': 0.0, 'city': 'string', 'rooms': 0,
+               'real_state_type': 'sale'}
+    driver = WebDriver()
+    global driverinstance
+    driverinstance = driver.driver_instance
+    url = "https://www.leboncoin.fr/"
+    solved = False
+    while not solved:
+        response = driverinstance.get('https://www.leboncoin.fr/_immobilier_/offres')
+        if response != None:
+            solved  = accept_cookies(driverinstance, url)
+        else:
+             break
+
+    time.sleep(5)
+    # driverinstance.find_element(By.XPATH, "//button[contains(@class,'_2qvLx _3WXWV')]").click()
+    # driverinstance.implicitly_wait(5)
+
+    print(driverinstance.current_url)
+    scrape_ads(driverinstance, 'https://www.leboncoin.fr/_immobilier_/offres')
+
