a5154b4692fe14a6572796624df168cfcb2cceb7
ritik <ritik@kifwat.com>
2022-06-18 15:09:55 +0530

    fix beicni

--- real_estate_advert/bienci/fetch.py
+++ real_estate_advert/bienci/fetch.py
@@ -6,98 +6,41 @@ import aiofiles
 import os
 cpath =os.path.dirname(__file__) 
 
-# from capachasolver_v2 import solveCapcha
-with open(f"{cpath}/cookies.json",'r') as file:
-    allcookie = file.read().splitlines() 
-cookie =allcookie
-count=0
-lastupdate= None
-updating = False
-def updataStatus(status):
-    with open("status.txt",'w') as file:
-        file.write(status)
 ua = fake_useragent.UserAgent(fallback='Your favorite Browser')
 def getUserAgent():
     return ua.random
 async def fetch(url,session,Json=False,file=False,**kwargs):
-    global cookie
-    global count
-    global updating
-    global lastupdate
-    
-    print("lenth of cooke +++++++++++++++++++++++", len(cookie))
-    if not cookie:
-        await asyncio.sleep(20)
-        cookie = allcookie
     if not kwargs.get('headers'):
         kwargs["headers"] = {
                 "user-agent":getUserAgent(),
                 }
-        try:
-            kwargs["headers"]['cookie'] = cookie[count]
-        except:
-            count = 0
-            kwargs["headers"]['cookie'] = cookie[count]
     try:
         # async with session.get(url,**kwargs) as response:
-            if file:
-                filename = str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '.')
-                path = "avendrealouerImg/"
-                filepath = path+filename
-                f = await aiofiles.open(filepath, mode='wb')
-                await f.write(await response.read())
-                await f.close()
-                return filepath
             try:
-                # response = await session.get(url,**kwargs,proxies={'https': 'socks4://80.81.232.145:5678'})
-                # response = await session.get(url,**kwargs,proxies={'https': 'socks4://127.0.0.1:9050'})
                 response = await session.get(url,**kwargs)
             except:
                 await asyncio.sleep(2)
                 return await fetch(url,session,Json,file,**kwargs)
-            # print()
-            # response = await session.get(url,proxies={'https': 'socks4://5.58.66.55:14888'})
-            # assert response.status == 200
-            # print(response)
             try:
-                updataStatus("200")
                 if response.status_code == 500:
                     return None
                 if response.status_code == 200:
                     if Json:
                         return  response.json()
-                    # doc = await response.text()
-                    # input('fetch input')
-                    # html = HTML(html=doc)
-                    # status =  response.status
                     print(url,response.status_code)
                     html = response.html
                     return html
                 else:
                     print(response.status_code,"in else fetch")
                     print(url,"in else fetch")
-                    updataStatus("403")
-                    # input('continue')
                     count +=1
-                    await asyncio.sleep(10)
-                    with open(f"{cpath}/cookies.json",'r') as file:
-                        cookie = file.read().splitlines()
-                    
-                    # time.sleep(5)
+                    await asyncio.sleep(1)
                     return await fetch(url,session,Json)
-                    
-                    # cookie = input("enter the cookies")
-                    # html = await fetch(session,url)
-                    # print(response.status)
-                    # with open("errorpagesfinal.txt",'a') as file:
-                    #     file.write(url+"\n")
-                    # return html
             except Exception as e:
                 print(response)
                 print(traceback.format_exc())
                 print(e,"got some isssue1")
                 # print(e)
-                input("some issue with that")
     except Exception as e:
         print(e,"got some isssue2")
         print(traceback.format_exc())
--- real_estate_advert/bienci/scraper.py
+++ real_estate_advert/bienci/scraper.py
@@ -1,13 +1,16 @@
 import asyncio
 import json
 import os
+import sys
+from urllib import response
+import requests
 from requests_html import AsyncHTMLSession
 import random
 try:
-    from fetch import fetch
+    from fetch import fetch,getUserAgent
     from uploader import AsyncKafkaTopicProducer
 except:
-    from .fetch import fetch
+    from .fetch import fetch,getUserAgent
     from .uploader import AsyncKafkaTopicProducer
 
 kafkaTopicName = "bienici_data_v1"
@@ -33,6 +36,94 @@ def getFirst(val):
         return val[0] 
     except:
         return None
+def syncfetch(session,url,Json=False,**kwargs):
+    kwargs['headers'] = {
+                "user-agent":getUserAgent(),
+                }
+    res = session.get(url,**kwargs)
+    if res.status_code == 200:
+        if Json:return res.json()
+        else:return res
+    else:
+        print(res.status_code)
+        return fetch(session,url,Json=Json,**kwargs)
+def getTotalResult(session,parameter):
+    parameter['sortBy'],parameter['sortOrder'],parameter["size"] = "price","desc",1
+    url = getFilterUrl(parameter)
+    res = syncfetch(session,url,Json=True)
+    totalres = res.get("total")
+    if totalres:return totalres
+    else:return 0
+def getMaxPrize(session,parameter):
+    parameter['sortBy'],parameter['sortOrder'],parameter["size"] = "price","desc",1
+    url = getFilterUrl(parameter)
+    res = syncfetch(session,url,Json=True)
+    totalres = res.get("realEstateAds")[0].get("price")
+    if totalres:return totalres
+    else:return 0
+def readGenFilter():
+    try:
+        with open("generatedPrizeFilter.json",'r') as file:
+            data = json.load(file)
+        return data
+    except:return {}
+def writeGenFilter(key,value):
+    prev = readGenFilter()
+    prev[key]=value
+    with open("generatedPrizeFilter.json",'w') as file:
+        file.write(json.dumps(prev))
+def genFilter(parameter,typ):
+    parameter["filterType"] = typ
+    session = requests.session()
+    dic = parameter
+    totalresult = getTotalResult(session,dic)
+    acres = totalresult
+    # dic['recherche[produit]']=typ
+    iniinterval = [0,37761]
+    maxprize = getMaxPrize(session,dic)
+    maxresult = 2400
+    filterurllist = ""
+    finalresult = 0
+    nooffilter = 0
+    retrydic = {iniinterval[0]:0}
+    while iniinterval[1]<=maxprize:
+        dic['minPrice'],dic['maxPrice'] = iniinterval
+        totalresult = getTotalResult(session,dic)
+        if (totalresult!=0 and maxresult-totalresult<=1400 and maxresult-totalresult>0) or (retrydic[iniinterval[0]]>10 and totalresult>0 and totalresult<maxresult):
+            # print("condition is stisfy going to next interval",totalresult)
+            # print(iniinterval,">apending")
+            filterurllist += json.dumps(iniinterval) + "/n/:"
+            asyncio.run(FetchFilter(dic))
+            iniinterval[0] = iniinterval[1]+1
+            iniinterval[1] = iniinterval[0]+int(iniinterval[0]/2)
+            finalresult +=totalresult
+            retrydic = {iniinterval[0]:0}
+            nooffilter +=1
+        elif maxresult-totalresult> 500:
+            # print("elif 1")
+            last = 10
+            iniinterval[1] = iniinterval[1] + int(iniinterval[1]/last)
+        elif totalresult == 0:
+            # print("elif 1",iniinterval)
+            last = 10
+            iniinterval[1] = iniinterval[1] + int(iniinterval[1]/last)
+        elif totalresult>maxresult:
+            # print("elif 2",iniinterval)
+            last = -5
+            dif = iniinterval[1]-iniinterval[0]
+            iniinterval[1] = iniinterval[1] + int(dif/-2) 
+            if iniinterval[0]>iniinterval[1]:
+                iniinterval[1] = iniinterval[0]+10
+        retrydic[iniinterval[0]] +=1
+        sys.stdout.write(f"\r{totalresult}-{maxresult}::::{acres} of  {finalresult}==>{iniinterval} no of filter {nooffilter}")
+        sys.stdout.flush()
+        # print(totalresult,"-",maxresult,"::::",acres ," of ", finalresult,"==>",iniinterval)
+    filterurllist+=json.dumps(iniinterval)
+    finalresult +=totalresult
+    print(finalresult,acres)
+    filterurllist = [json.loads(query) for query in filterurllist.split("/n/:")]
+    writeGenFilter(typ,filterurllist)
+
 asyncpage = 10
 # change the url of filter data to scrap all the ads
 async def main():
@@ -57,7 +148,6 @@ async def main():
     "sortBy":"relevance",
     "sortOrder":"desc",
     "onTheMarket":[True],
-    "zoneIdsByTypes":{"zoneIds":["-126506","-126513","-126516","-126525","-126523","-126514","-126519","-126512","-126528","-121747","-126515","-126520","-126508","-126529"]}
     }
     for pin,zonid in citys:
         if lastpin == pin:
@@ -87,6 +177,7 @@ async def saveRealstateAds(ads,**kwargs):
     #     file.write(allads)
 
 async def GetAllPages(baseurl,session,first=False,Filter=None,**kwargs):
+    print(Filter)
     r= await fetch(baseurl,session,Json=True)
     if r:
         print(baseurl,"-200")
@@ -96,15 +187,53 @@ async def GetAllPages(baseurl,session,first=False,Filter=None,**kwargs):
             totalpage = int(totalpage)+1 if totalpage>int(totalpage) else int(totalpage)
             print(len(r["realEstateAds"]),r['total'],"tis is ")
             tasks =[]
+            print(totalpage,r['total'],Filter['size'])
+            input("chekd the pages")
             for i in range(2,totalpage):
                 Filter['from'] += Filter['size'] 
                 url = getFilterUrl(Filter)
-                tasks.append(asyncio.ensure_future(GetAllPages(baseurl,session,first=True,Filter=Filter,**kwargs)))
+                tasks.append(asyncio.ensure_future(GetAllPages(baseurl,session,first=False,Filter=Filter,**kwargs)))
             await asyncio.gather(*tasks)
+
+async def FetchFilter(filters):
+    filters['size'] = 500
+    session = AsyncHTMLSession()
+    baseurl = getFilterUrl(filters)
+    producer = AsyncKafkaTopicProducer()
+    await producer.statProducer()
+    await GetAllPages(baseurl,session,first=True,Filter=filters,producer=producer)
+    await producer.stopProducer()
 
 
 def main_scraper():
-    asyncio.run(main())
+    # asyncio.run(main())
+    param  = {
+    "size":500,
+    "from":0,
+    "showAllModels":False,
+    "filterType":"buy",
+    "propertyType":["house","flat"],
+    "newProperty":False,
+    "page":3,
+    "sortBy":"relevance",
+    "sortOrder":"desc",
+    "onTheMarket":[True],
+    }
+    genFilter(param,typ="buy")
+    genFilter(param,typ="rent")
 if __name__ == "__main__":
-    asyncio.run(main())
-
+    # asyncio.run(main())
+    param  = {
+    "size":500,
+    "from":0,
+    "showAllModels":False,
+    "filterType":"buy",
+    "propertyType":["house","flat"],
+    "newProperty":False,
+    "page":3,
+    "sortBy":"relevance",
+    "sortOrder":"desc",
+    "onTheMarket":[True],
+    }
+    genFilter(param,typ="buy")
+    genFilter(param,typ="rent")
