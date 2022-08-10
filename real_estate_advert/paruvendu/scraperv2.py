b6e62b6004dd7e5cfc977da8aa17e6791a538938
ritik <ritik@kifwat.com>
2022-08-10 16:40:27 +0530

    fix paruvendu parser

--- real_estate_advert/paruvendu/scraperv2.py
+++ real_estate_advert/paruvendu/scraperv2.py
@@ -43,7 +43,7 @@ async def savedata(resjson,**kwargs):
     ads = resjson['feed']["row"]
     producer = kwargs["producer"]
     await producer.TriggerPushDataList('paruvendu-data_v1',ads)
-    await producer.TriggerPushDataList('common-ads-data_v1',[ParseParuvendu[ad] for ad in ads])
+    await producer.TriggerPushDataList('common-ads-data_v1',[ParseParuvendu(ad) for ad in ads])
     # for ad in ads:
     #     resstr += json.dumps(ad)+"\n"
     # with open("output.json",'a') as file:
