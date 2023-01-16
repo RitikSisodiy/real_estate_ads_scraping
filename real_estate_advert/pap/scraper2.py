abdec2d5ef74c3139e1f3fb51da5bc31f3055f26
ritik <ritik@kifwat.com>
2023-01-16 17:25:45 +0530

    fix

--- real_estate_advert/pap/scraper2.py
+++ real_estate_advert/pap/scraper2.py
@@ -31,6 +31,7 @@ class PapScraper:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
         }
+
         self.prox = ProxyScraper(SELOGER_SECURITY_URL,headers)
         try:
             self.proxies = self.readProxy()
