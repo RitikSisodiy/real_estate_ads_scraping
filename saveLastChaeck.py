89524ceebb098d8fb49be365a90fd0d867ea05d5
ritik <ritik@kifwat.com>
2023-03-28 12:41:40 +0530

    fix

--- saveLastChaeck.py
+++ saveLastChaeck.py
@@ -21,53 +21,60 @@ commonIdUpdate = "common-ads-portal-lastcheck"
 # This function is used to update the last check time of all ads for a given website.
 def saveLastCheck(website,nowtime):
     producer = AsyncKafkaTopicProducer()
-     # creating an update query to mark all ads with a last_check time less than nowtime as inactive
-    update_query = {
-        "script": {
-            "source": "ctx._source.available=false",
-            "lang": "painless"
-        },
-        "query":{
-            "bool":{
-                "must":[
-                    {
-                        "match": {
-                            "website": website
-                        }
-                    },
-                    {
-                        "range": {
-                            "last_checked": {
-                                "lte": nowtime
+    try:
+        # creating an update query to mark all ads with a last_check time less than nowtime as inactive
+        update_query = {
+            "script": {
+                "source": "ctx._source.available=false",
+                "lang": "painless"
+            },
+            "query":{
+                "bool":{
+                    "must":[
+                        {
+                            "match": {
+                                "website": website
+                            }
+                        },
+                        {
+                            "range": {
+                                "last_checked": {
+                                    "lte": nowtime
+                                }
                             }
                         }
-                    }
-                ]
-                
+                    ]
+                    
+                }
             }
         }
-    }
-    # checking the messages behind the Kafka topic consumer group before updating the ads
-    data = {
-        "website":website,
-        "lastcheck":nowtime,
-    }
-    producer.PushDataList(commonIdUpdate,[data])
-    while True:
-        topic = f"activeid-{website}"
-        if checkBehindMessages(topic,session)<=0:break
-        time.sleep(10)
-    # updating the ads in Elasticsearch using the update_by_query API
-    while True:
-        try:
-            es = Elasticsearch(**cred)
-            response = es.update_by_query(index=commonIndex, body=update_query,wait_for_completion=False)
-            es.close()
-            break
-        except:pass
-        finally:es.close()
+        # checking the messages behind the Kafka topic consumer group before updating the ads
+        data = {
+            "website":website,
+            "lastcheck":nowtime,
+        }
+        producer.PushDataList(commonIdUpdate,[data])
+        while True:
+            topic = f"activeid-{website}"
+            if checkBehindMessages(topic,session)<=0:break
+            time.sleep(10)
+        # updating the ads in Elasticsearch using the update_by_query API
+        while True:
+            try:
+                es = Elasticsearch(**cred)
+                response = es.update_by_query(index=commonIndex, body=update_query,wait_for_completion=False)
+                es.close()
+                break
+            except:pass
+            finally:es.close()
 
-    # pushing a message to the common-ads-portal-lastcheck Kafka topic with the updated last check time information   
-    data["task_id"] =response['task']
-    producer.PushDataList(commonIdUpdate,[data])
-    
+        # pushing a message to the common-ads-portal-lastcheck Kafka topic with the updated last check time information   
+        data["task_id"] =response['task']
+        producer.PushDataList(commonIdUpdate,[data])
+    except Exception as e:
+        data = {
+            "portal":"website",
+            "nowtime":nowtime,
+            "error":str(e)
+        }
+        producer.PushDataList(commonIdUpdate,[data])
