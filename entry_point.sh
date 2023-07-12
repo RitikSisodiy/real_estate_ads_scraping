5325454ff7232bf0fb16ee3366fbde0079b3ea0e
ritiksisodiy <ritik.s10120@gmail.com>
2023-07-12 17:37:49 +0530

    fix entrypoint .sh

--- entry_point.sh
+++ entry_point.sh
@@ -1,6 +1,6 @@
 sh -c  redis-server --daemonize yes \
-uvicorn main:app --host 0.0.0.0 --port 8000 \
+&& ( uvicorn main:app --host 0.0.0.0 --port 8000 \
 & celery -A tasks.celery_app flower --host=0.0.0.0 --port=38000 \
 & celery -A tasks.celery_app worker --loglevel=info \
 & celery -A tasks.celery_app beat -l info \
-& java -jar real_estate_advert/seloger/selger.jar
+& java -jar real_estate_advert/seloger/selger.jar )
