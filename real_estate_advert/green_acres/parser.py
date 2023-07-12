273a84debb75d35b817972e3166d0636200cafcd
Shubham <salmonhumorous@gmail.com>
2023-07-12 16:13:57 +0530

    enhance green_acres

--- real_estate_advert/green_acres/parser.py
+++ real_estate_advert/green_acres/parser.py
@@ -1,77 +1,130 @@
 from datetime import datetime
+
+
 def ParseGreenAcre(data):
-  try:
-    now = datetime.now()
-    assetsDic = {info["type"]:info["value"]  for info in data.get("characteristics")}
-    area = assetsDic.get(0) or assetsDic.get(9) or 0
-    area = int(area)
-    landarea = assetsDic.get(9) or 0
-    sellerInfo = data.get("agencyAccount")
-    images = data.get("largePics")
-    sdata = {
-      "id": data.get("id"),
-      "ads_type": "buy",
-      "price": data.get("price") or 0,
-      "original_price": data.get("originalPrice") or data.get("price") or 0,
-      "area": area ,
-      "city": data.get("location"),     # "Poitiers (86000)"
-      "declared_habitable_surface": area,
-      "declared_land_surface": landarea,
-      "land_surface": landarea,
-      "declared_rooms":  assetsDic.get(1) or 0,
-      "declared_bedrooms":  assetsDic.get(2) or 0,
-      "rooms": assetsDic.get(1) or 0,
-      "bedrooms": assetsDic.get(2) or 0,
-      "longitude": data.get("latitude"),
-      "latitude": data.get("longitude"),
-      "location": f'{data.get("latitude")}, {data.get("longitude")}',
-      "title": data.get("title"),
-      "description": data.get('summary'),
-      "postal_code": data.get("postalCode") or "",
-      "agency": sellerInfo.get("accountType") == "agency",
-      "agency_name": sellerInfo.get("agencyName"),
-      "agency_details": {
-        "logo": sellerInfo.get("advertiserDisplayInfo").get("advertiserLogoUrl"),
-        "address":sellerInfo.get("address"),
-        "name":sellerInfo.get("agencyName"),
-        "rcs":sellerInfo.get("siret"),
-        "phone":sellerInfo.get("phone") or "" ,
-        "email":sellerInfo.get("email"),
-        "website":sellerInfo.get("url"),
-        "city_name":sellerInfo.get("city"),
-        "zipcode":sellerInfo.get("postalCode"),
-        },
-      "available": True,
-      "status": True,
-      "furnished": any(word in (data.get("summary").lower() + " ".join(data.get("options") or "")) for word in ["furnished","meublée","meublé"]),
-      "coloc_friendly": False,
-      "elevator": any(word in (data.get("summary").lower() + " ".join(data.get("options") or "")) for word in ["elevator","ascenseur"]),
-      "pool": any(word in (data.get("summary").lower() + " ".join(data.get("options") or "")) for word in ["piscine","piscina"]),
-      "balcony": any(word in (data.get("summary").lower() + " ".join(data.get("options") or "")) for word in ["balcony","balcon",]),
-      "floor": any(word in (data.get("summary").lower() + " ".join(data.get("options") or "")) for word in ["estage","floor","étage"]),
-      "terrace": any(word in (data.get("summary").lower() + " ".join(data.get("options") or "")) for word in ["terrace","terrasse"]),
-      "insee_code": data.get("postalCode") or "",
-      "parking": any(word in (data.get("summary").lower() + " ".join(data.get("options") or "")) for word in ["parking","garage"]),
-      "images_url": images,
-      "is_new": False,
-      "website": "green-acres.fr",
-      "property_type": data.get("type"),
-      "published_at": getTimeStamp(data.get("creationDate")),
-      "created_at": getTimeStamp(data.get("lastAdvertUpdate")),
-      "last_modified": getTimeStamp(data.get("lastAdvertUpdate")),
-      "visite_virtuelle":data.get("visual3DUrl") or f'https://www.youtube.com/watch?v={data.get("videoLink")}' if data.get("videoLink") else "" ,
-      "others":{
-        "assets":[f'{info["formattedText"]}:{info["value"]}' for info in data.get("characteristics")] if  data.get("characteristics") else []
-      },
-      "url": f"https://www.green-acres.fr/fr/properties/{data.get('id')}.htm",
-      "dpe": "N/A",
-      "ges": "N/A",
-      "last_checked": now.isoformat(),
-      }
-    
-    return sdata
-  except:
-    open("greenAcer.json",'w').write(json.dumps(data)+"\n")
-    return {}
-    traceback.print_exc()
-    raise ValidationErr
+    """
+    Parses the `data` dictionary and extracts relevant information to create a new dictionary.
+
+    Args:
+    - data: A dictionary containing the data to be parsed.
+
+    Returns:
+    - sdata: A dictionary containing the extracted information from `data`.
+      If an error occurs during parsing, it returns an empty dictionary.
+    """
+    try:
+        now = datetime.now()
+        assetsDic = {
+            info["type"]: info["value"] for info in data.get("characteristics")
+        }
+        area = assetsDic.get(0) or assetsDic.get(9) or 0
+        area = int(area)
+        landarea = assetsDic.get(9) or 0
+        sellerInfo = data.get("agencyAccount")
+        images = data.get("largePics")
+        sdata = {
+            "id": data.get("id"),
+            "ads_type": "buy",
+            "price": data.get("price") or 0,
+            "original_price": data.get("originalPrice") or data.get("price") or 0,
+            "area": area,
+            "city": data.get("location"),  # "Poitiers (86000)"
+            "declared_habitable_surface": area,
+            "declared_land_surface": landarea,
+            "land_surface": landarea,
+            "declared_rooms": assetsDic.get(1) or 0,
+            "declared_bedrooms": assetsDic.get(2) or 0,
+            "rooms": assetsDic.get(1) or 0,
+            "bedrooms": assetsDic.get(2) or 0,
+            "longitude": data.get("latitude"),
+            "latitude": data.get("longitude"),
+            "location": f'{data.get("latitude")}, {data.get("longitude")}',
+            "title": data.get("title"),
+            "description": data.get("summary"),
+            "postal_code": data.get("postalCode") or "",
+            "agency": sellerInfo.get("accountType") == "agency",
+            "agency_name": sellerInfo.get("agencyName"),
+            "agency_details": {
+                "logo": sellerInfo.get("advertiserDisplayInfo").get(
+                    "advertiserLogoUrl"
+                ),
+                "address": sellerInfo.get("address"),
+                "name": sellerInfo.get("agencyName"),
+                "rcs": sellerInfo.get("siret"),
+                "phone": sellerInfo.get("phone") or "",
+                "email": sellerInfo.get("email"),
+                "website": sellerInfo.get("url"),
+                "city_name": sellerInfo.get("city"),
+                "zipcode": sellerInfo.get("postalCode"),
+            },
+            "available": True,
+            "status": True,
+            "furnished": any(
+                word
+                in (data.get("summary").lower() + " ".join(data.get("options") or ""))
+                for word in ["furnished", "meublée", "meublé"]
+            ),
+            "coloc_friendly": False,
+            "elevator": any(
+                word
+                in (data.get("summary").lower() + " ".join(data.get("options") or ""))
+                for word in ["elevator", "ascenseur"]
+            ),
+            "pool": any(
+                word
+                in (data.get("summary").lower() + " ".join(data.get("options") or ""))
+                for word in ["piscine", "piscina"]
+            ),
+            "balcony": any(
+                word
+                in (data.get("summary").lower() + " ".join(data.get("options") or ""))
+                for word in [
+                    "balcony",
+                    "balcon",
+                ]
+            ),
+            "floor": any(
+                word
+                in (data.get("summary").lower() + " ".join(data.get("options") or ""))
+                for word in ["estage", "floor", "étage"]
+            ),
+            "terrace": any(
+                word
+                in (data.get("summary").lower() + " ".join(data.get("options") or ""))
+                for word in ["terrace", "terrasse"]
+            ),
+            "insee_code": data.get("postalCode") or "",
+            "parking": any(
+                word
+                in (data.get("summary").lower() + " ".join(data.get("options") or ""))
+                for word in ["parking", "garage"]
+            ),
+            "images_url": images,
+            "is_new": False,
+            "website": "green-acres.fr",
+            "property_type": data.get("type"),
+            "published_at": getTimeStamp(data.get("creationDate")),
+            "created_at": getTimeStamp(data.get("lastAdvertUpdate")),
+            "last_modified": getTimeStamp(data.get("lastAdvertUpdate")),
+            "visite_virtuelle": data.get("visual3DUrl")
+            or f'https://www.youtube.com/watch?v={data.get("videoLink")}'
+            if data.get("videoLink")
+            else "",
+            "others": {
+                "assets": [
+                    f'{info["formattedText"]}:{info["value"]}'
+                    for info in data.get("characteristics")
+                ]
+                if data.get("characteristics")
+                else []
+            },
+            "url": f"https://www.green-acres.fr/fr/properties/{data.get('id')}.htm",
+            "dpe": "N/A",
+            "ges": "N/A",
+            "last_checked": now.isoformat(),
+        }
+
+        return sdata
+    except:
+        open("greenAcer.json", "w").write(json.dumps(data) + "\n")
+        return {}
--- real_estate_advert/green_acres/scraper.py
+++ real_estate_advert/green_acres/scraper.py
@@ -3,18 +3,19 @@ import traceback
 import aiohttp
 import asyncio
 import os
-from requests_html import HTMLSession,HTML
-import json,re
+from requests_html import HTMLSession, HTML
+import json, re
 from .parser import ParseGreenAcre
 from HttpRequest.uploader import AsyncKafkaTopicProducer
+
 kafkaTopicName = "green-acres-data_v1"
 commonTopicName = "common-ads-data_v1"
 
-s= HTMLSession()
-pagesize  = 100 # maxsize is 100
-cpath =os.path.dirname(__file__) or "."
+s = HTMLSession()
+pagesize = 100  # maxsize is 100
+cpath = os.path.dirname(__file__) or "."
 url = "https://apifront.green-acres.com/api/v8.0/visitors/registered/listings/search"
-params = json.load(open(cpath+"/filter.json"))
+params = json.load(open(cpath + "/filter.json"))
 headers = {
     "Accept": "application/problem+json",
     "Content-Type": "application/json",
@@ -23,24 +24,23 @@ headers = {
     "Accept-Language": "en",
     "Host": "apifront.green-acres.com",
     "Connection": "Keep-Alive",
-    "Accept-Encoding": "gzip"
+    "Accept-Encoding": "gzip",
 }
-async def fetch(session,url,method="get",jsonresponce=True,**kwargs):
+
+
+async def fetch(session, url, method="get", jsonresponce=True, **kwargs):
     header = headers
-    if kwargs.get('headers'):
+    if kwargs.get("headers"):
         header = kwargs["headers"]
         del kwargs["headers"]
-    # if params:
-    #     query_string = urllib.parse.urlencode( params )
-    #     url += "?"+query_string 
     try:
-        if method=="get":
-            res = await session.get(url,headers = header,**kwargs)
-        if method =="post":
-            res = await session.post(url,headers = header,**kwargs)
+        if method == "get":
+            res = await session.get(url, headers=header, **kwargs)
+        if method == "post":
+            res = await session.post(url, headers=header, **kwargs)
     except Exception as e:
         await asyncio.sleep(3)
-        return await fetch(session,url,method,jsonresponce,**kwargs)
+        return await fetch(session, url, method, jsonresponce, **kwargs)
     if jsonresponce:
         response = await res.json()
         return response
@@ -48,212 +48,202 @@ async def fetch(session,url,method="get",jsonresponce=True,**kwargs):
         content = await res.text()
         html = HTML(html=content)
         return html
+
+
 def getTimeStamp(strtime):
-    formate = '%Y-%m-%dT%H:%M:%S'
-    # 2022-06-19T05:26:55
-    t = datetime.strptime(strtime,formate)
+    formate = "%Y-%m-%dT%H:%M:%S"
+    t = datetime.strptime(strtime, formate)
     return int(t.timestamp())
-async def getDeription(session,id):
+
+
+async def getDeription(session, id):
     url = f"https://www.green-acres.fr/fr/properties/{id}.htm"
-    r = await fetch(session,url,jsonresponce=False,headers={
-        "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
-    })
-    des = r.find("#DescriptionDiv .text",first=True).text
+    r = await fetch(
+        session,
+        url,
+        jsonresponce=False,
+        headers={
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+        },
+    )
     return des
 
-async def savedata(session,ads,**kwargs):
+
+async def savedata(session, ads, **kwargs):
     print(len(ads))
     producer = kwargs["producer"]
-    # await producer.TriggerPushDataList(kafkaTopicName,ads)
-    # await producer.TriggerPushDataList('common-ads-data_v1',[ParseLefigaro(ad) for ad in ads])
     dif = 10
-    for i in range(0,len(ads),dif):
-        a = i//dif
-        tads =[] 
-        for ad in ads[a*dif:(a*dif)+dif]:
-            if len(ad["summary"])==0:
-                ad["summary"] = await getDeription(session,ad["id"])
+    for i in range(0, len(ads), dif):
+        a = i // dif
+        tads = []
+        for ad in ads[a * dif : (a * dif) + dif]:
+            if len(ad["summary"]) == 0:
+                ad["summary"] = await getDeription(session, ad["id"])
             tads.append(ad)
-        await producer.TriggerPushDataList(kafkaTopicName,tads)
+        await producer.TriggerPushDataList(kafkaTopicName, tads)
         ads = [ParseGreenAcre(ad) for ad in tads]
-        await producer.TriggerPushDataList(commonTopicName,ads)
-        # resstr = ''
-        # for ad in tads:
-        #     if len(ad["summary"])==0:
-        #         print("gettin des")
-        #         ad["summary"] = await getDeription(session,ad["id"])
-        #     resstr += json.dumps(ad)+"\n"
-        # with open("output.json",'a') as file:
-        #     file.write(resstr)
-async def startCrawling(session,param,**kwargs):
-    # for param in filterParamList:
-        param["searchConfiguration"]["pageSize"] = 1
-        data = await fetch(session,url,method="post",json=param)
-        await savedata(session,data["result"]["adverts"],**kwargs)
-        totalres = int(data["result"]["totalAdverts"])
-        totalpage = (totalres//19)+1
-        param["searchConfiguration"]["pageSize"] = totalpage
-        totalpage = int(totalpage) if totalpage==int(totalpage) else int(totalpage)+1
-        print(totalres,param)
-        tasks = []
-        print(totalpage,"this is total pages")
-        for i in range(1,totalpage+1):
-            param["searchConfiguration"].update({"pageNumber":i})
-            # print(param)
-            await parstItems(session,param,page=i,**kwargs)
-            # tasks.append(asyncio.ensure_future(parstItems(session,param,page=i,**kwargs)))
-        await asyncio.gather(*tasks)  
-        # totaldata = 0
-        # for d in data:
-        #     results = len(d['feed']["row"])
-        #     print(results,"this is len of rows")
-        #     totaldata += results
-        #     await savedata(d,**kwargs)
-        # print(totaldata)
-async def getTotalResult(session,params,url):
+        await producer.TriggerPushDataList(commonTopicName, ads)
+
+
+async def startCrawling(session, param, **kwargs):
+    param["searchConfiguration"]["pageSize"] = 1
+    data = await fetch(session, url, method="post", json=param)
+    await savedata(session, data["result"]["adverts"], **kwargs)
+    totalres = int(data["result"]["totalAdverts"])
+    totalpage = (totalres // 19) + 1
+    param["searchConfiguration"]["pageSize"] = totalpage
+    totalpage = int(totalpage) if totalpage == int(totalpage) else int(totalpage) + 1
+    print(totalres, param)
+    tasks = []
+    print(totalpage, "this is total pages")
+    for i in range(1, totalpage + 1):
+        param["searchConfiguration"].update({"pageNumber": i})
+        await parstItems(session, param, page=i, **kwargs)
+    await asyncio.gather(*tasks)
+
+
+async def getTotalResult(session, params, url):
     totalres = {
-        'pageSize':1,
+        "pageSize": 1,
     }
     params.update(totalres)
     print(params)
-    r = await fetch(session,url,method="post",json=params)
+    r = await fetch(session, url, method="post", json=params)
     return int(r["result"]["totalAdverts"])
-async def getMaxPrize(session,params,url):
-    # dic,burl = GetUrlFilterdDict(url)
-    # dic["ajaxAffinage"] = 0
-    # dic["ddlTri"] = "prix_seul"
-    # dic["ddlOrd"] = "desc"
-    prizefilter = {
-        "sort":7,
-        "pageSize":1
-    }
+
+
+async def getMaxPrize(session, params, url):
+    prizefilter = {"sort": 7, "pageSize": 1}
     params.update(prizefilter)
-    r = await fetch(session,url,params=params)
+    r = await fetch(session, url, params=params)
     prize = r["classifieds"][0]["priceLabel"]
-    prize = float(re.search("[0-9.]+",prize).group())
+    prize = float(re.search("[0-9.]+", prize).group())
     return prize
-async def parstItems(session,param,page=None,save=True,**kwargs):
+
+
+async def parstItems(session, param, page=None, save=True, **kwargs):
     if page:
-        param.update({"pageNumber":page})
-    data = await fetch(session,url,method="post",json=param)
-    # print(param['p'])
-    # data = json.load(res)
+        param.update({"pageNumber": page})
+    data = await fetch(session, url, method="post", json=param)
     if save:
-        await savedata(session,data["result"]["adverts"],**kwargs)
+        await savedata(session, data["result"]["adverts"], **kwargs)
         return None
     return data
+
+
 async def CheckId(id):
     headers = {
-        "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"
     }
     async with aiohttp.ClientSession() as session:
-        furl  = f"https://fi-classified-search-api.immo.fcms.io/apps/classifieds?paId={id}&showdetail=1&mobId=dFlRt5PY0Gg:APA91bHEo1pJb5ChJsqkD-kzsxabz7I3tE8UctG_yN9_Do7_3QQM5ecUvw9jJln3Tm4UxghOmk4H2jozt9dZ8QFu2KuDWwc16av2QQ3SZOVCInP6TB5af9xoW2m_tvpc885HY4JZqsmd&key=lafNgtmagb6VrZugp7Wim2SUf32gZtask2iomq+lo891jsyq2N2Sx9+dfZnaqpm3lqZ+l6qDn8F1opyRmIODsWbGxIXb3GrPr9KimbrSlbermZ+HnqCaqmOd1KzZa5a+abKnpZewpqm83ZeXnMbXlISDaouLhLyleqG1aLl0Z8WXmtmfvJeexNncpMmenZaqjGaBkqKo08ZUVpZommZkmmSTmmCIiKPKy6eY2KaZwpSohW6tqG2pypKh"
-        # print(furl)
-        r = await fetch(session,furl,headers=headers)
-        # print(r)
+        furl = f"https://fi-classified-search-api.immo.fcms.io/apps/classifieds?paId={id}&showdetail=1&mobId=dFlRt5PY0Gg:APA91bHEo1pJb5ChJsqkD-kzsxabz7I3tE8UctG_yN9_Do7_3QQM5ecUvw9jJln3Tm4UxghOmk4H2jozt9dZ8QFu2KuDWwc16av2QQ3SZOVCInP6TB5af9xoW2m_tvpc885HY4JZqsmd&key=lafNgtmagb6VrZugp7Wim2SUf32gZtask2iomq+lo891jsyq2N2Sx9+dfZnaqpm3lqZ+l6qDn8F1opyRmIODsWbGxIXb3GrPr9KimbrSlbermZ+HnqCaqmOd1KzZa5a+abKnpZewpqm83ZeXnMbXlISDaouLhLyleqG1aLl0Z8WXmtmfvJeexNncpMmenZaqjGaBkqKo08ZUVpZommZkmmSTmmCIiKPKy6eY2KaZwpSohW6tqG2pypKh"
+        r = await fetch(session, furl, headers=headers)
         data = r
         print(len(data["feed"]["row"]))
-        if data and len(data["feed"]["row"])==1:
+        if data and len(data["feed"]["row"]) == 1:
             return True
-        else:return False
-async def main(adsType = ""):
-    # catid info
-    # vente is for  Vente immobilier 
-    # location is for Location immobilier
-    # filterParamList = [*getFilter(param) for param in params
+        else:
+            return False
+
+
+async def main(adsType=""):
     async with aiohttp.ClientSession() as session:
-        await CreatelastupdateLog(session,adsType)
+        await CreatelastupdateLog(session, adsType)
         producer = AsyncKafkaTopicProducer()
-        await startCrawling(session,params,producer=producer)
-        # await startCrawling(session,filterParamList,producer=producer)
+        await startCrawling(session, params, producer=producer)
         await producer.stopProducer()
+
+
 def main_scraper(payload):
     adtype = payload["real_state_type"]
     if adtype == "Updated/Latest Ads":
         UpdateGreenacres()
     else:
         asyncio.run(main(adtype))
+
+
 def getLastUpdates():
     try:
-        with open(f'{cpath}/lastUpdate.json','r') as file:
+        with open(f"{cpath}/lastUpdate.json", "r") as file:
             updates = json.load(file)
     except:
         return {}
     return updates
+
+
 async def GetAdUpdate(ad):
     updates = {}
     modifiedtime = ad.get("lastAdvertUpdate") or ad.get("creationDate")
     id = ad.get("id")
-    nowtime  = datetime.now()
+    nowtime = datetime.now()
     updates = {
-        "timestamp":nowtime.timestamp(),
-        "lastupdate":getTimeStamp(modifiedtime),
-        "lastadId":id,
+        "timestamp": nowtime.timestamp(),
+        "lastupdate": getTimeStamp(modifiedtime),
+        "lastadId": id,
     }
     return updates
 
-async def CreatelastupdateLog(session,typ):
+
+async def CreatelastupdateLog(session, typ):
     updates = getLastUpdates()
 
-    params["searchConfiguration"].update({
-        'sortOrder':"data_d",
-        'pageSize':1
-        })
+    params["searchConfiguration"].update({"sortOrder": "data_d", "pageSize": 1})
     print(params)
-    d = await fetch(session,url,method="post",json=params)
+    d = await fetch(session, url, method="post", json=params)
     try:
-        data = d['result']['adverts'][0]
+        data = d["result"]["adverts"][0]
         latupdate = await GetAdUpdate(data)
-        # print(latupdate)
-        updates.update({typ:latupdate})
+        updates.update({typ: latupdate})
     except Exception as e:
         traceback.print_exc()
-        print("execption ======>" , e)
+        print("execption ======>", e)
 
-    # lastupdate = json.load(open(f'{cpath}/lastUpdate.json','r'))
     print(updates)
-    with open(f'{cpath}/lastUpdate.json','w') as file:
+    with open(f"{cpath}/lastUpdate.json", "w") as file:
         file.write(json.dumps(updates))
+
 
 async def asyncUpdateGreenacres():
     updates = getLastUpdates()
-    # print(updates)
     async with aiohttp.ClientSession() as session:
         if not updates:
-            await CreatelastupdateLog(session,'sale')
+            await CreatelastupdateLog(session, "sale")
         updates = getLastUpdates()
-        for key,val in updates.items():
-            await CreatelastupdateLog(session,key)
+        for key, val in updates.items():
+            await CreatelastupdateLog(session, key)
             if key == "rental":
                 catid = "vente"
             else:
                 catid = "location"
-            params.update({
-            'sort':5,
-            'transaction':catid,
-            'pageSize':100,
-            'showdetail':1
-            })
+            params.update(
+                {"sort": 5, "transaction": catid, "pageSize": 100, "showdetail": 1}
+            )
             updated = False
-            p=1
+            p = 1
             producer = AsyncKafkaTopicProducer()
-            while not updated and p<=100:
+            while not updated and p <= 100:
                 print(f"cheking page {p}")
-                adsres = await parstItems(session,params,page=p,save=False,producer=producer)
+                adsres = await parstItems(
+                    session, params, page=p, save=False, producer=producer
+                )
                 ads = adsres["classifieds"]
-                lastad = ads[len(ads)-1]
+                lastad = ads[len(ads) - 1]
                 webupdates = await GetAdUpdate(lastad)
-                await savedata(session,adsres,producer=producer)
-                res = val['lastupdate']>webupdates['lastupdate']
-                print(f"{val['lastupdate']}>{webupdates['lastupdate']} ={res} and type {key}")
+                await savedata(session, adsres, producer=producer)
+                res = val["lastupdate"] > webupdates["lastupdate"]
+                print(
+                    f"{val['lastupdate']}>{webupdates['lastupdate']} ={res} and type {key}"
+                )
                 if res:
                     updated = True
-                p+=1
+                p += 1
             await producer.stopProducer()
+
 
 def UpdateGreenacres():
     asyncio.run(asyncUpdateGreenacres())
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     url = "https://fi-classified-search-api.immo.fcms.io/apps/classifieds"
     asyncio.run(main())
-
